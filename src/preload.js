6e448b8afc2bfca062661b63bddaa8b331fc37e1
Bazyli Brzóska <bbrzoska@zendesk.com>
2021-04-09 01:08:16 -0700

    next

--- src/preload.js
+++ src/preload.js
@@ -69,12 +69,10 @@ const onReady = async () => {
   disconnectEl.addEventListener("click", () => {
     console.log("disconnecting");
     disposed = true
-    // device.disconnect();
+    device.disconnect();
   });
 
   const audioContext = new AudioContext();
-
-
   await ready;
 
   // console.log(await navigator.mediaDevices.enumerateDevices())
@@ -82,78 +80,13 @@ const onReady = async () => {
     console.error("Error capturing audio.");
   });
 
-  // function show_some_data(given_typed_array, num_row_to_display, label) {
-  //   let size_buffer = given_typed_array.length;
-  //   let index = 0;
-  //   let max_index = num_row_to_display;
-
-  //   console.log("__________ " + label);
-
-  //   for (; index < max_index && index < size_buffer; index += 1) {
-  //     console.log(given_typed_array[index]);
-  //   }
-  // }
-
-  // function process_microphone_buffer(event) {
-  //   // invoked by event loop
-
-  //   let i, N, inp, microphone_output_buffer;
-
-  //   microphone_output_buffer = event.inputBuffer.getChannelData(0); // just mono - 1 channel for now
-
-  //   // microphone_output_buffer  <-- this buffer contains current gulp of data size BUFF_SIZE
-
-  //   show_some_data(microphone_output_buffer, 5, "from getChannelData");
-  // }
-
   function start_microphone(stream) {
-    // let gain_node = audioContext.createGain();
-    // gain_node.connect(audioContext.destination);
-
     let microphone_stream = audioContext.createMediaStreamSource(stream);
-    // microphone_stream.connect(gain_node);
-    // let BUFF_SIZE = 16384;
-
-    // let script_processor_node = audioContext.createScriptProcessor(
-    //   BUFF_SIZE,
-    //   1,
-    //   1
-    // );
-    // script_processor_node.addEventListener(
-    //   "audioprocess",
-    //   process_microphone_buffer
-    // );
-
-    // microphone_stream.connect(script_processor_node);
-
-    // --- enable volume control for output speakers
-
-    // document.getElementById("volume").addEventListener("change", function () {
-    //   let curr_volume = this.value;
-    //   gain_node.gain.value = curr_volume;
-
-    //   console.log("curr_volume ", curr_volume);
-    // });
-
-    // --- setup FFT
-
-    // let script_processor_fft_node = audioContext.createScriptProcessor(
-    //   2048,
-    //   1,
-    //   1
-    // );
-    // script_processor_fft_node.connect(gain_node);
-
     let analyserNode = audioContext.createAnalyser();
     analyserNode.smoothingTimeConstant = 0;
     analyserNode.fftSize = 32; // 2048
 
     microphone_stream.connect(analyserNode);
-
-    // analyserNode.connect(script_processor_fft_node);
-
-    // script_processor_fft_node.addEventListener("audioprocess", function () {
-    // get the average for the first channel
     let data = new Uint8Array(analyserNode.frequencyBinCount);
 
     const loopingFunction = throttle(() => {
@@ -163,16 +96,10 @@ const onReady = async () => {
       if (data.length) {
         // console.log(average * 10)
         updateLight([...data]);
-
-        // show_some_data(data, 5, "from fft");
       }
     }, 100);
 
     loopingFunction();
-
-    // draw the spectrogram
-    // if (microphone_stream.playbackState == microphone_stream.PLAYING_STATE) {
-    // });
   }
 };
 
